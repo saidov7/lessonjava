5d935aa6700863af8b3e00a9ccf3c587c5be82ad
saidov7 <jaxasaidov29@gmail.com>
2025-03-08 16:46:55 +0500

    first commit

---
+++ java.js
@@ -0,0 +1,47 @@
+// let gradus = prompt("Ob-havo gradusini kiriting:");
+
+// if (gradus <= -20) {
+//   alert("Qahraton");
+// } else if (gradus > -20 && gradus <= 0) {
+//   alert("Juda sovuq");
+// } else if (gradus > 0 && gradus <= 10) {
+//   alert("Sovuqqina havo");
+// } else if (gradus > 10 && gradus <= 20) {
+//   alert("Salqin havo");
+// } else if (gradus > 20 && gradus <= 30) {
+//   alert("Iliq havo");
+// } else if (gradus > 30 && gradus <= 40) {
+//   alert("Issiq havo");
+// } else if (gradus > 40) {
+//   alert("Jazirama");
+// }
+
+
+// let son = prompt("Son kiriting:");
+
+// if (son % 3 === 0 && son % 5 === 0) {
+//   alert("FizzBuzz");
+// } else if (son % 3 === 0) {
+//   alert("Fizz");
+// } else if (son % 5 === 0) {
+//   alert("Buzz");
+// } else {
+//   alert(son);
+// }
+
+
+// let son1 = Number(prompt("Birinchi sonni kiriting:"));
+// let operator = prompt("Amalni kiriting (+, -, *, /):");
+// let son2 = Number(prompt("Ikkinchi sonni kiriting:"));
+
+// if (operator === "+") {
+//   alert(son1 + son2);
+// } else if (operator === "-") {
+//   alert(son1 - son2);
+// } else if (operator === "*") {
+//   alert(son1 * son2);
+// } else if (operator === "/") {
+//   alert(son1 / son2);
+// } else {
+//   alert("Noto'g'ri amal kiritildi.");
+// }
